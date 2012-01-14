170d562f06d4084cf061f6deb10ffa67fea8679d
dataLAB <info@datalabprojects.com>
2012-01-14 11:06:15 -0500

    Removed unused tests

--- spec/artworker/fractions_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe Artworker::Fractions do
-  
-  it "should return a fraction from a decimal"
-  
-end
--- spec/artworker/titles_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe Artworker::Titles do
-  
-  it "should return the artwork title with date"
-  
-  it "should return the artwork title (italicized) with date"
-  
-end
