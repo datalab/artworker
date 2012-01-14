b32d36072ec643d3da33cbb20bd494ca1c612932
dataLAB <info@datalabprojects.com>
2012-01-14 12:33:15 -0500

    Initial gh-pages creation

--- spec/models.rb
+++
@@ -1,8 +0,0 @@
-class ArtworkModels < ActiveRecord::Base
-  uses_artworker_artwork :dimensions => { :dimensions => [:height, :width, :depth], 
-                                          :framed_dimensions => [:framed_height, :framed_width, :framed_depth]}
-end
-
-class ArtistModels < ActiveRecord::Base
-  uses_artworker_artist
-end
